ed822efc3115e727d3939e349d3fe7cb248e3b46
Rebecca G. Estes <estes.rebecca@gmail.com>
2015-11-13 11:29:57 -0500

    need to fix edit authentification

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,41 +1,52 @@
 class PinsController < ApplicationController
 
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@pins = Pin.all
 	end
 
 	def new
+		@user = current_user
 		@pin = Pin.new
 	end
 
 	def create
-		@pin = Pin.create(pin_params)
+		@user = current_user
+		@pin = Pin.create(pin_params.merge(user: @user))
 		redirect_to pin_path(@pin)
 	end
 
 	def show
-		@pin = Pin.find(params[:id])
+		# @user = @pin.user
 	end
 
 	def edit
-		@pin = Pin.find(params[:id])
+		@user = @pin.user
+		if @user != current_user
+			flash[:alert] = "Access denied! You can't edit someone else's post."
+			redirect_to root_path
+		end
 	end
 
 	def update
-		@pin = Pin.find(params[:id])
-		@pin.update(pin_params)
+		@pin.update(pin_params.merge(user: @user))
 		redirect_to pin_path(@pin)
 	end
 
 	def destroy
-		@pin = Pin.find(params[:id])
 		@pin.destroy
 		redirect_to pins_path
 	end
 
 	private
 	def pin_params
-		params.require(:pin).permit(:title, :img_url)
+		params.require(:pin).permit(:user_id, :title, :img_url)
+	end
+
+	def set_post
+		@pin = Pin.find(params[:id])
 	end
 
 end
