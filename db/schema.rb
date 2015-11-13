bb5bcfe56c0d5cb034c9b3f94455c34288c3b3d8
Alice Frumin <alifrumin@yahoo.com>
2015-11-13 13:24:09 -0500

    need to delete

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113142444) do
+ActiveRecord::Schema.define(version: 20151113180609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string  "name"
+    t.integer "user_id"
+  end
 
   create_table "pins", force: :cascade do |t|
     t.datetime "created_at", null: false
