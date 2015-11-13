a54cb7ff58c47f69c616ed65e435b2312a2f7446
Rebecca G. Estes <estes.rebecca@gmail.com>
2015-11-13 10:38:32 -0500

    crud functionality

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -17,6 +17,22 @@ class PinsController < ApplicationController
 		@pin = Pin.find(params[:id])
 	end
 
+	def edit
+		@pin = Pin.find(params[:id])
+	end
+
+	def update
+		@pin = Pin.find(params[:id])
+		@pin.update(pin_params)
+		redirect_to pin_path(@pin)
+	end
+
+	def destroy
+		@pin = Pin.find(params[:id])
+		@pin.destroy
+		redirect_to pins_path
+	end
+
 	private
 	def pin_params
 		params.require(:pin).permit(:title, :img_url)
